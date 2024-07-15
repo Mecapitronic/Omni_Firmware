fbdc0b642b29c6633f0d5b4cd468d130a4648f80
Geoffroy Arenou <geoarenou@gmail.com>
2024-07-15 15:05:30 +0200

    Test on arduino nano every with lib khoih-prog/ContinuousStepper_Generic@^1.0.0

--- include/pin.h
+++ include/pin.h
@@ -14,4 +14,31 @@
 #define stepPinM3 8
 #define dirPinM3  7
 
+
+// Use with Stepper-Motor driver, such as TMC2209
+
+#define _PWM_LOGLEVEL_        4
+
+#include "megaAVR_PWM.h"
+
+//#define USING_TIMERB        true
+
+#if USING_TIMERB
+  // Pins tested OK in Nano Every ATmega4809
+  #define STEP_PIN      3            // TimerB1, for higher frequencies, up to 100KHz
+  //#define STEP_PIN      6            // TimerB0, for higher frequencies, up to 100KHz
+#elif USING_ARDUINO_MEGA_AVR_CORE
+  // Pins tested OK in Nano Every ATmega4809 using Arduino megaAVR core
+  // TimerA0 somehow can't be used with MegaCoreX
+  #define STEP_PIN      5            // TimerA0, only accurate @ low frequencies (< 1KHz) because of low 250KHz clock
+  //#define STEP_PIN      9            // TimerA0, only accurate @ low frequencies (< 1KHz) because of low 250KHz clock
+  //#define STEP_PIN     10            // TimerA0, only accurate @ low frequencies (< 1KHz) because of low 250KHz clock
+#else
+  #error TimerA0 to be used with Arduino megaAVR Core
+#endif
+
+#define DIR_PIN       4
+
+
+
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,22 @@
 #include "main.h"
 
+megaAVR_PWM* stepper;
+
+void setSpeed(int speed)
+{
+  if (speed == 0)
+  {
+    // Use DC = 0 to stop stepper
+    stepper->setPWM(STEP_PIN, 1000, 0);
+  }
+  else
+  {
+    //  Set the frequency of the PWM output and a duty cycle of 50%
+    digitalWrite(DIR_PIN, (speed < 0));
+    stepper->setPWM(STEP_PIN, abs(speed), 50);
+  }
+}
+
 void setup()
 {
   Serial.end();
@@ -8,6 +25,11 @@ void setup()
   Serial.println("Robot Holonome Firmware");
   delay(1000);
 
+  double theta = 150 * PI / 180;      // Angle Ref X+
+  double theta1 = theta + 0;          // Angle for motor 1
+  double theta2 = theta + 2 * PI / 3; // Angle for motor 2 (120 degrees in radians)
+  double theta3 = theta + 4 * PI / 3; // Angle for motor 3 (240 degrees in radians)
+
   // Sets the two pins as Outputs
   pinMode(stepPinM1, OUTPUT);
   pinMode(dirPinM1, OUTPUT);
@@ -15,6 +37,16 @@ void setup()
   pinMode(dirPinM2, OUTPUT);
   pinMode(stepPinM3, OUTPUT);
   pinMode(dirPinM3, OUTPUT);
+  
+  pinMode(DIR_PIN, OUTPUT);
+  
+  Serial.print(F("\nStarting PWM_StepperControl on "));
+  Serial.println(BOARD_NAME);
+  Serial.println(MEGA_AVR_PWM_VERSION);
+  
+  // Create PWM object and passed just a random frequency of 500
+  // The duty cycle is how you turn the motor on and off
+  stepper = new megaAVR_PWM(STEP_PIN, 500, 0);
 }
 
 void loop()
@@ -77,4 +109,36 @@ void loop()
     delayMicroseconds(500);
   }
   delay(1000);
+
+
+  for (long x = 0; x <= 5000; x+=10)
+  {
+
+    setSpeed(x);
+    delay(2);
+  }
+  for (long x = 5000; x >= 0; x-=10)
+  {
+
+    setSpeed(x);
+    delay(2);
+  }
+
+    setSpeed(0);
+/*
+  setSpeed(2000);
+  delay(3000);
+
+  // Stop before reversing
+  setSpeed(0);
+  delay(3000);
+
+  // Reversing
+  setSpeed(-5000);
+  delay(3000);
+
+  // Stop before reversing
+  setSpeed(0);
+  delay(3000);
+*/
 }
