84ddc939a501c97ecc004267775834285ff014c8
Geoffroy Arenou <geoarenou@gmail.com>
2025-03-22 14:48:07 +0100

    Unit test with unity by PIO
    Co-authored-by: Perceval <perceval.arenou@gmail.com>

---
+++ test/test_path_finding.cpp
@@ -0,0 +1,47 @@
+#include <unity.h>
+#include <Arduino.h>
+
+// TEST_ASSERT_X( {modifiers}, {expected}, actual, {size/count} )
+
+
+void setUp(void) {
+    // set stuff up here
+}
+
+void tearDown(void) {
+    // clean stuff up here
+} 
+
+void test_function_should_do(void) {
+    // test stuff
+    int8_t expected = 3;
+    int8_t result = 3; 
+    TEST_ASSERT_EQUAL_INT8(expected, result);
+;}
+
+void test_function_true(void) {
+    TEST_ASSERT_TRUE(true);
+}
+
+void test_function_fail(void) {
+    TEST_ASSERT_TRUE(false);
+}
+
+// execute all test functions
+int runUnityTests(void) {
+    UNITY_BEGIN();
+    RUN_TEST(test_function_should_do);
+    RUN_TEST(test_function_fail);
+    RUN_TEST(test_function_true);
+    return UNITY_END();
+  }
+
+void setup() {
+    // Wait ~2 seconds before the Unity test runner
+    // establishes connection with a board Serial interface
+    delay(2000);
+  
+    runUnityTests();
+}
+
+void loop() {}
