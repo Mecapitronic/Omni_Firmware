68c5b544b8bdd9bc8da63f49fc3a86c7d176fe87
PercevalSA <perceval.arenou@gmail.com>
2025-03-25 09:51:06 +0000

    use fastled to turn on led ring and fix build flags

--- src/LedRGB.cpp
+++ src/LedRGB.cpp
@@ -1,39 +1,22 @@
 #include "LedRGB.h"
 
-void LedRGB::Initialisation(uint8_t numPixels, uint8_t pin, uint8_t brightness)
+// uint8_t brightness)
+void LedRGB::init(uint8_t numPixels, uint8_t data_pin)
 {
     print("RGB initialisation of ", numPixels, " pixels");
-    println(" on pin ", pin);
-    pixels = Adafruit_NeoPixel(numPixels, pin, NEO_RGB + NEO_KHZ800);
-    pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
-    pixels.setBrightness(brightness);
-    pixels.clear();
+    println(" on pin ", data_pin);
+    m_numPixels = numPixels;
+
+    FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, numPixels);
+    FastLED.setBrightness(RGB_BRIGHTNESS);
 }
 
-void LedRGB::Update()
+void LedRGB::update()
 {
-    static int i = 0;
-    if (i > color.capacity())
-        i = 0;
-    pixels.fill(color[i++]);
-    pixels.show();
-    // println("RGB Fill");
-}
-
-void LedRGB::HandleCommand(Command cmd)
-{
-    // if (cmd.cmd == "RGB")
-    //{
-    //  RGB:0;0;0
-    //    print("RGB : ", cmd.data[0]);
-    //    print(" : ", cmd.data[1]);
-    //    print(" : ", cmd.data[2]);
-    //    println();
-    //}
-}
-
-void LedRGB::PrintCommandHelp()
-{
-    Printer::println("RGB Command Help :");
-    Printer::println("! No Command yet !");
+    leds[0] = CRGB::White;
+    FastLED.show();
+    delay(30);
+    leds[0] = CRGB::Black;
+    FastLED.show();
+    delay(30);
 }
